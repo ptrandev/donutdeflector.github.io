8aee980c438821dbea7b3a811dc648934c8a13f7
DonutDeflector <donutdeflector@tuta.io>
2017-04-09 15:46:34 -0400

    uncss setup and new css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var uncss = require('gulp-uncss');
 
 gulp.task('css', function() {
-  return gulp.src('assets/css/main.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(cleanCSS({level: '2'}))
-    .pipe(gulp.dest('dist'));
+    return gulp.src(['assets/css/*.css'])
+      .pipe(uncss({html: ['*.html', 'contact/*.html', 'projects/*.html', 'projects/**/*.html']}))
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(cleanCSS({level: '2'}))
+      .pipe(gulp.dest('dist'));
 });
