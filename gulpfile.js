098904c539294fdb7972076ba8db05367ac24a82
DonutDeflector <donutdeflector@tuta.io>
2017-04-09 21:27:16 -0400

    gulp automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var unCSS = require('gulp-uncss');
 var cleanCSS = require('gulp-clean-css');
-var uncss = require('gulp-uncss');
+
+gulp.task('concat', function() {
+    gulp.src(['assets/css/source/*.css'])
+      .pipe(shell(['cat assets/css/source/*.css > assets/css/source/development/main.min.css']));
+});
 
 gulp.task('css', function() {
-    return gulp.src(['assets/css/*.css'])
-      .pipe(uncss({html: ['*.html', 'contact/*.html', 'projects/*.html', 'projects/**/*.html']}))
+    return gulp.src(['assets/css/source/development/main.min.css'])
+      .pipe(unCSS({html: ['*.html', 'contact/*.html', 'projects/*.html', 'projects/**/*.html']}))
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(cleanCSS({level: '2'}))
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('assets/css/source/*.css', ['concat']);
+    gulp.watch('assets/css/source/development/*', ['css']);
 });
