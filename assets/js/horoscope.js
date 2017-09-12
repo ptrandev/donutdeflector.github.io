a0af973fa92502ed7030a79e5b78a813d42fbe1c
Phillip T <donutdeflector@protonmail.com>
2017-09-12 14:29:38 -0400

    Update horoscope.js

--- assets/js/horoscope.js
+++ assets/js/horoscope.js
@@ -1,25 +1,55 @@
 jQuery(function () {
-  console.log("Welcome the to the Javascript Horoscope Generator!");
 
+  // Input Box Border Animation
   $("#answers").focus(function() {
-    $(this).animate({borderWidth: '5px'}, '500', 'swing');
+    $(this).animate({borderWidth: '5px'}, 100, 'linear');
   });
 
   $("#answers").focusout(function() {
-    $(this).animate({borderWidth: '2px'}, '500', 'swing');
+    $(this).animate({borderWidth: '2px'}, 100, 'swing');
   });
 
+  // Global Variables
+  let questionNumber = 0;
 
+  // Question Numerator Function
+  const questionNumerator = () => {
+    questionNumber = questionNumber + 1;
+    return questionNumber;
+  }
 
-  /* Month Function */
-  const birthDay = () => {
-    let month = document.getElementById('answers').value.toLowerCase();
+  // Birth Month Function
+  const birthMonth = () => {
+    month = document.getElementById('answers').value.toLowerCase();
     return month;
   }
 
+  // Birth Day Function
+  const birthDay = () => {
+    day = document.getElementById('answers').value.toLowerCase();
+    return day;
+  }
+
+  // Display Question Function
+  const displayQuestion = () => {
+    if (questionNumber === 0) {
+      document.getElementById('question').innerHTML = "What month were you born in?";
+    } else if (questionNumber === 1) {
+      document.getElementById('question').innerHTML = "What day were you born in?";
+    } else if (questionNumber === 2) {
+      document.getElementById('question').innerHTML = `You were born ${month} ${day}, right?`;
+    } else {
+      document.getElementById('question').innerHTML = "null";
+    }
+  }
+
   $('#answers').keydown(function (e) {
-      console.log('key', e.which)
       if (e.which == 13) {
+        console.log(birthMonth());
+        console.log(questionNumerator());
+        displayQuestion();
       }
   });
+
+  displayQuestion();
 });
