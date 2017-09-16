06df4b84d6d9635e07de244d41933bddd7ce03a6
Phillip T <donutdeflector@protonmail.com>
2017-09-16 15:20:26 -0400

    Update horoscope.js

--- assets/js/horoscope.js
+++ assets/js/horoscope.js
@@ -32,12 +32,19 @@ jQuery(function () {
     return day;
   }
 
-  // Confirmation Function
-  const userConfirmation = () => {
-    confirmation = document.getElementById('answers').value.toLowerCase();
-    if (confirmation === "no") {
+  // conformation Function
+  const userconformation = () => {
+    conformation = document.getElementById('answers').value.toLowerCase();
+
+    if (conformation === "no" || conformation === "no." || conformation === "n") {
       questionNumber = 0;
       return questionNumber;
+    } else {
+      horoscopeOptions();
+      useAorAn();
+      horoscopeInformation();
+      $("#answers").fadeOut(400);
+      $(".horoscope-information").delay(400).fadeIn(400);
     }
   }
 
@@ -47,8 +54,7 @@ jQuery(function () {
       if (day >= 1 && day <= 19) {
         horoscope = "Capricorn";
         return horoscope;
-      }
-      if (day >= 20 && day <= 31) {
+      } else if (day >= 20 && day <= 31) {
         horoscope = "Aquarius";
         return horoscope;
       }
@@ -56,8 +62,7 @@ jQuery(function () {
       if (day >= 1 && day <= 18) {
         horoscope = "Aquarius";
         return horoscope;
-      }
-      if (day >= 19 && day <= 28) {
+      } else if (day >= 19 && day <= 28) {
         horoscope = "Pisces";
         return horoscope;
       }
@@ -65,8 +70,7 @@ jQuery(function () {
       if (day >= 1 && day <= 20) {
         horoscope = "Pisces";
         return horoscope;
-      }
-      if (day >= 21 && day <= 31) {
+      } else if (day >= 21 && day <= 31) {
         horoscope = "Aries";
         return horoscope;
       }
@@ -74,8 +78,7 @@ jQuery(function () {
       if (day >= 1 && day <= 19) {
         horoscope = "Aries";
         return horoscope;
-      }
-      if (day >= 20 && day <= 30) {
+      } else if (day >= 20 && day <= 30) {
         horoscope = "Taurus";
         return horoscope;
       }
@@ -83,8 +86,7 @@ jQuery(function () {
       if (day >= 1 && day <= 20) {
         horoscope = "Taurus";
         return horoscope;
-      }
-      if (day >= 21 && day <= 31) {
+      } else if (day >= 21 && day <= 31) {
         horoscope = "Gemini";
         return horoscope;
       }
@@ -92,8 +94,7 @@ jQuery(function () {
       if (day >= 1 && day <= 21) {
         horoscope = "Gemini";
         return horoscope;
-      }
-      if (day >= 22 && day <= 30) {
+      } else if (day >= 22 && day <= 30) {
         horoscope = "Cancer";
         return horoscope;
       }
@@ -101,8 +102,7 @@ jQuery(function () {
       if (day >= 1 && day <= 22) {
         horoscope = "Cancer";
         return horoscope;
-      }
-      if (day >= 23 && day <= 31) {
+      } else if (day >= 23 && day <= 31) {
         horoscope = "Leo";
         return horoscope;
       }
@@ -110,8 +110,7 @@ jQuery(function () {
       if (day >= 1 && day <= 22) {
         horoscope = "Leo";
         return horoscope;
-      }
-      if (day >= 23 && day <= 31) {
+      } else if(day >= 23 && day <= 31) {
         horoscope = "Virgo";
         return horoscope;
       }
@@ -119,8 +118,7 @@ jQuery(function () {
       if (day >= 1 && day <= 22) {
         horoscope = "Virgo";
         return horoscope;
-      }
-      if (day >= 23 && day <= 30) {
+      } else if (day >= 23 && day <= 30) {
         horoscope = "Libra";
         return horoscope;
       }
@@ -128,8 +126,7 @@ jQuery(function () {
       if (day >= 1 && day <= 22) {
         horoscope = "Libra";
         return horoscope;
-      }
-      if (day >= 23 && day <= 31) {
+      } else if (day >= 23 && day <= 31) {
         horoscope = "Scorpio";
         return horoscope;
       }
@@ -137,8 +134,7 @@ jQuery(function () {
       if (day >= 1 && day <= 21) {
         horoscope = "Scorpio";
         return horoscope;
-      }
-      if (day >= 22 && day <= 30) {
+      } else if (day >= 22 && day <= 30) {
         horoscope = "Sagittarius";
         return horoscope;
       }
@@ -146,8 +142,7 @@ jQuery(function () {
       if (day >= 1 && day <= 21) {
         horoscope = "Sagittarius";
         return horoscope;
-      }
-      if (day >= 22 && day <= 31) {
+      } else if (day >= 22 && day <= 31) {
         horoscope = "Aquarius";
         return horoscope;
       }
@@ -173,10 +168,87 @@ jQuery(function () {
     if (horoscope === "Aries") {
       document.getElementById('element').innerHTML = "Fire";
       document.getElementById('lucky-numbers').innerHTML = "1, 8, 17";
-      document.getElementById('strengths').innerHTML = "Courage, Confidence, Determination, Enthusiasm, Optimism, Honesty, Passion";
+      document.getElementById('strengths').innerHTML = "Courage, Confidence, Determination, Enthusiasm, Optimism, Honesty, and Passion";
       document.getElementById('weaknesses').innerHTML = "Impatience, Moodiness, Short-Tempered, Impulsiveness, and Agressiveness";
       document.getElementById('likes').innerHTML = "Comfy Clothes, Being in Charge, Pysical Challenges, and Individual Sports";
       document.getElementById('dislikes').innerHTML = "Inactivity, Delays, and Work That Doesn't Use Your Talents";
+    } else if (horoscope === "Taurus") {
+      document.getElementById('element').innerHTML = "Earth";
+      document.getElementById('lucky-numbers').innerHTML = "2, 6, 9, 12, 24";
+      document.getElementById('strengths').innerHTML = "Reliability, Patience, Practicality, Devotion, Responsibility, and Stability";
+      document.getElementById('weaknesses').innerHTML = "Stubbornness, Possesiveness, and Being Uncompromising";
+      document.getElementById('likes').innerHTML = " Gardening, Cooking, Music, Romance, High Quality Clothing, and Working With Your Hands";
+      document.getElementById('dislikes').innerHTML = "Sudden Change, Complications, Insecurity, and Synthetic Fabric";
+    } else if (horoscope === "Gemini") {
+      document.getElementById('element').innerHTML = "Air";
+      document.getElementById('lucky-numbers').innerHTML = "5, 7, 13, 23";
+      document.getElementById('strengths').innerHTML = "Gentleness, Affection, Curiosity, Adaptability, and Ability to Learn Quickly and Exchange Ideas";
+      document.getElementById('weaknesses').innerHTML = "Nervousness, Inconsistency, and Indecisiveness";
+      document.getElementById('likes').innerHTML = "Music, Books, Magazines, and Chatting";
+      document.getElementById('dislikes').innerHTML = "Being Alone, Confinement, Repetition and and Routine";
+    } else if (horoscope === "Cancer") {
+      document.getElementById('element').innerHTML = "Water";
+      document.getElementById('lucky-numbers').innerHTML = "2, 3, 15, 20";
+      document.getElementById('strengths').innerHTML = "Tenacity, Imagination, Loyalty, Emotions, Sympathy, and Persuasion";
+      document.getElementById('weaknesses').innerHTML = "Moodiness, Pessimism, Suspiciousness, Manipulativeness, and Insecurity";
+      document.getElementById('likes').innerHTML = "Art, Home-Based Hobbies, Relaxing Near Water, Helping Loved Ones, and A Good Meal With Friends";
+      document.getElementById('dislikes').innerHTML = "Strangers, Revealing Personal Life, and Criticism of Loved Ones";
+    } else if (horoscope === "Leo") {
+      document.getElementById('element').innerHTML = "Fire";
+      document.getElementById('lucky-numbers').innerHTML = "1, 3, 10, 19";
+      document.getElementById('strengths').innerHTML = "Creativity, Passion, Generosity, Warm-Heartedness, Cheerfulness, and Humour";
+      document.getElementById('weaknesses').innerHTML = "Arrogance, Stubbornness, Being Self-Centered, Laziness, and Inflexibility";
+      document.getElementById('likes').innerHTML = "Theatre, Holidays, Being Admired, Expensive Things, Bright Colors, and Having Fun With Friends";
+      document.getElementById('dislikes').innerHTML = "Being Ignored, Facing Reality, and Not Being Pampered";
+    } else if (horoscope === "Virgo") {
+      document.getElementById('element').innerHTML = "Earth";
+      document.getElementById('lucky-numbers').innerHTML = "5, 14, 15, 23, 32";
+      document.getElementById('strengths').innerHTML = "Loyalty, Analysing, Kindness, Hardworking, and Practicality";
+      document.getElementById('weaknesses').innerHTML = "Shyness, Worrying, and Being Overly Crtical";
+      document.getElementById('likes').innerHTML = "Animals, Healthy Food, Books, Nature, and Cleanliness";
+      document.getElementById('dislikes').innerHTML = "udeness, Asking for Help, and Being The Center of Attention";
+    } else if (horoscope === "Libra") {
+      document.getElementById('element').innerHTML = "Air";
+      document.getElementById('lucky-numbers').innerHTML = "4, 6, 13, 15, 24";
+      document.getElementById('strengths').innerHTML = "Cooperation, Diplomacy, Graciousness, Fair-Mindedness, and Sociability";
+      document.getElementById('weaknesses').innerHTML = "Indecisiveness, Holding Grudges, and Being Self-Pitying";
+      document.getElementById('likes').innerHTML = "Harmony, Gentleness, Sharing, and the Outdoors";
+      document.getElementById('dislikes').innerHTML = "Violence, Injustice, Loudmouths, and Conformity";
+    } else if (horoscope === "Scorpio") {
+      document.getElementById('element').innerHTML = "Water";
+      document.getElementById('lucky-numbers').innerHTML = "8, 11, 18, 22";
+      document.getElementById('strengths').innerHTML = "Resourcefulness, Braveness, Passion, Stubbornness, and Friendship";
+      document.getElementById('weaknesses').innerHTML = "Being Distrusting, Jealousy, Secretiveness, and Violence";
+      document.getElementById('likes').innerHTML = "Truth, Facts, Being Right, Longtime Friends, Teasing, and Grand Passions";
+      document.getElementById('dislikes').innerHTML = "Dishonesty, Revealing Secrets, and Passivity";
+    } else if (horoscope === "Sagittarius") {
+      document.getElementById('element').innerHTML = "Fire";
+      document.getElementById('lucky-numbers').innerHTML = "3, 7, 9, 12, 21";
+      document.getElementById('strengths').innerHTML = "Generosity, Idealism, and Humour";
+      document.getElementById('weaknesses').innerHTML = "Impatience, Being Outspoken, and Promising More Than They Can Deliver";
+      document.getElementById('likes').innerHTML = "Freedom, Trave, Philosophy, and Being Outdoors";
+      document.getElementById('dislikes').innerHTML = "Clinginess, Being Constrained, Conspiracy Theories, and Details";
+    } else if (horoscope === "Capricorn") {
+      document.getElementById('element').innerHTML = "Earth";
+      document.getElementById('lucky-numbers').innerHTML = "4, 8, 13, 22";
+      document.getElementById('strengths').innerHTML = "Responsibility, Discipline, Self-Control, and Managing Others";
+      document.getElementById('weaknesses').innerHTML = "Being Know-It-Alls, Being Unforgiving, Condescension, and Pessimism";
+      document.getElementById('likes').innerHTML = "Family, Tradition, Music, Understatement, and Quality Craftsmanship";
+      document.getElementById('dislikes').innerHTML = "Just About Anything Eventually";
+    } else if (horoscope === "Aquarius") {
+      document.getElementById('element').innerHTML = "Air";
+      document.getElementById('lucky-numbers').innerHTML = "4, 7, 11, 22, 29";
+      document.getElementById('strengths').innerHTML = "Progressiveness, Originality, Independence, and Humanitarianism";
+      document.getElementById('weaknesses').innerHTML = "Being Tempermental, Being Uncompromising, Aloofness, and Avoiding Emotional Expression";
+      document.getElementById('likes').innerHTML = "Having Fun With Friends, Helping Others, Fighting For a Cause, Intellectual Conversation, and Listeners";
+      document.getElementById('dislikes').innerHTML = "Limitations, Broken Promises, Being Lonely, Dullness, and Boredom";
+    } else if (horoscope === "Pisces") {
+      document.getElementById('element').innerHTML = "Water";
+      document.getElementById('lucky-numbers').innerHTML = "3, 9, 12, 15, 18, 24";
+      document.getElementById('strengths').innerHTML = "Compassion, Art, Intuition, Gentleness, Wisdom, and Music";
+      document.getElementById('weaknesses').innerHTML = "Fearfulness, Being Overly Trusting, Sadness, and Escapism";
+      document.getElementById('likes').innerHTML = "Being Alone, Sleeping, Music, Romance, Visual Media, Swimming, and Spirituality";
+      document.getElementById('dislikes').innerHTML = "Know-It-Alls, Criticism, Living in the Past, and Cruelty";
     }
   }
 
@@ -184,17 +256,10 @@ jQuery(function () {
   const variableStorer = () => {
     if (questionNumber === 1) {
       birthMonth();
-    }
-    if (questionNumber === 2) {
+    } else if (questionNumber === 2) {
       birthDay();
-    }
-    if (questionNumber === 3) {
-      userConfirmation();
-      horoscopeOptions();
-      useAorAn();
-      horoscopeInformation();
-      $("#answers").fadeOut(400);
-      $(".horoscope-information").delay(400).fadeIn(400);
+    } else if (questionNumber === 3) {
+      userconformation();
     }
   }
 
@@ -205,16 +270,16 @@ jQuery(function () {
     } else if (questionNumber === 1) {
       document.getElementById('question').innerHTML = "What day were you born in?";
     } else if (questionNumber === 2) {
-      document.getElementById('question').innerHTML = `You were born <span class="bold">${month}  ${day}</span>, right?`;
+      document.getElementById('question').innerHTML = `You were born <span class="bold"> ${month} ${day}</span>, right?`;
     } else if (questionNumber === 3) {
-      document.getElementById('question').innerHTML = `You are ${aoran} <span class="bold" id="horoscope-name">${horoscope}</span>. Check below for more information!`;
+      document.getElementById('question').innerHTML = `You are ${aoran} <span class="bold" id="horoscope-name"> ${horoscope}</span>. Check below for more information!`;
     } else {
       document.getElementById('question').innerHTML = "null";
     }
   }
 
   // Runs Functions on Enter
-  $('#answers').keydown(function (e) {
+  $('#answers').keydown(function(e) {
       if (e.which == 13) {
         console.log(questionNumerator());
         console.log(variableStorer());
@@ -224,6 +289,15 @@ jQuery(function () {
         displayQuestion();
       }
   });
+
+  $('#restart').click(function() {
+    $(".horoscope-information").fadeOut(400);
+    $("#answers").delay(400).fadeIn(400);
+
+    questionNumber = 0;
+
+    displayQuestion();
+  })
 
   // Display First Question
   displayQuestion();
